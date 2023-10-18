e6a6aa0a67133771969ce9fd229c41a8e5eafe47
dinhtrivonguyen@ggmail.com <dinhtrivonguyen@gmail.com>
2023-10-19 02:33:50 +0700

    update

--- src/Board.js
+++ src/Board.js
@@ -180,7 +180,7 @@ export default function Board() {
       <p className="instructions">
         Hướng dẫn chơi trò chơi tô màu theo số thứ tự:
         <br />
-        1. Nhấn vào các mảnh hoa để tô màu chúng theo thứ tự từ 1 đến 7. 
+        1. Nhấn vào các mảnh hoa để tô màu chúng theo thứ tự từ 1 đến 7.
         <br />
         2. Chọn màu sắc từ bảng màu hàng ngang bên dưới và tô màu vào các mảnh
         hoa.
@@ -188,7 +188,8 @@ export default function Board() {
         3. Khi tất cả mảnh hoa đã được tô màu đúng thứ tự, bạn sẽ hoàn thành trò
         chơi.
         <br />
-        Gợi ý: Hãy nhấn vào số 1 và sau đó nhấn vào cánh hoa đang nhấp nháy để tô màu cánh hoa.
+        Gợi ý: Hãy nhấn vào số 1 và sau đó nhấn vào cánh hoa đang nhấp nháy để
+        tô màu cánh hoa.
       </p>
 
       <div className="picture mb-4 d-flex justify-content-center">
