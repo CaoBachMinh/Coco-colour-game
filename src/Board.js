5b363704d6be9f9775fc0df78cedbb676be25f9b
normalman159 <binben279@gmail.com>
2023-10-28 00:18:22 +0700

    1

--- src/Board.js
+++ src/Board.js
@@ -1,11 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import "./App.css";
 import Button from "./Color_Button";
-<<<<<<< Updated upstream
-
-=======
 import Board_lv1 from "./Board_lv1";
->>>>>>> Stashed changes
 export default function Board() {
   
   const [curcolor,setcurcolor] = useState(null);
